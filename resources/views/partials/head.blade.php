c3f55ec5cb67fcd0e4506d8b20653091887025d9
s3a317104 <31909567+s3a317104@users.noreply.github.com>
2017-11-28 22:25:51 +0800

    Partials

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
+<script type="text/javascript" src="{{ asset('js/jquery.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
